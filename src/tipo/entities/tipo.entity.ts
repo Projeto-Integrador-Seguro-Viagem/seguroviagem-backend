de8f80be160c072e747eaa19232d073e8db06c34
michaellflorencio <michael.lima.florencio@gmail.com>
2025-12-17 16:27:52 -0300

    .

--- src/tipo/entities/tipo.entity.ts
+++ src/tipo/entities/tipo.entity.ts
@@ -1,11 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
-<<<<<<< HEAD
-import { Contrato } from '../../contratos/entities/contratos.entity';
-import { Usuario } from '../../usuarios/entities/usuarios.entity';
-=======
 import { Usuario } from '../../usuarios/entities/usuarios.entity';
 import { Apolice } from '../../apolices/entities/apolices.entity';
->>>>>>> 12bbb22fd7dd349677e8814b2102fb699b1834ee
 
 @Entity({ name: 'tb_tipo' })
 export class Tipo {
