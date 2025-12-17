e1eafea41ee41b01d783f24a6e44036ff99f90d0
michaellflorencio <michael.lima.florencio@gmail.com>
2025-12-17 13:36:14 -0300

    Implementando o TIPO

---
+++ src/tipo/entities/tipo.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
+import { Contrato } from '../../contratos/entities/contratos.entity';
+
+@Entity({ name: 'tb_tipo' })
+export class Tipo {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length: 255 })
+    nome: string; // internacional / nacional / EUA
+
+    @Column({ type: 'text' })
+    descricao: string;
+
+    @ManyToOne(() => Usuario, (usuario) => usuario.tipos, { onDelete: 'CASCADE' })
+    @JoinColumn({ name: 'usuario_id' })
+    usuario: Usuario;
+
+    @ManyToOne(() => Contrato, (contrato) => contrato.tipos, { onDelete: 'CASCADE' })
+    @JoinColumn({ name: 'contrato_id' })
+    contrato: Contrato;
+}
